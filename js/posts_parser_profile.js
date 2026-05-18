0e305ff4be289e0605b800701d7c8fc8c0ef6d58
Maksym <63255725+UnnamedPers@users.noreply.github.com>
2026-05-18 11:43:00 +0200

    feat: implement dynamic post rendering on profile page using new posts_parser_profile.js and updated CSS grid layout

--- js/posts_parser_profile.js
+++ js/posts_parser_profile.js
@@ -15,8 +15,9 @@ fetch("product-list.json")
 function spawnPosts(data) {
     items_grid.innerHTML = "";
     for (let i = 0; i < data.length; i++) {
-        const postCard = document.createElement("div");
+        const postCard = document.createElement("a");
         postCard.classList.add("post_container");
+        postCard.href = "post.html";
         postCard.innerHTML = `
         <div class="post_images">
             <div class="image-cover">
@@ -32,22 +33,11 @@ function spawnPosts(data) {
                 <div class="black_shadow"></div>
             </div>
         </div>
-        <div class="post_profile">
-            <div class="student_profile_pic" style="background-image: url('${data[i].student_profile_pic}');"></div>
-            <p class="captions">@${data[i].post_username}</p>
-        </div>
         <h2 class="post_heading">${data[i].item_title}</h2>
         <div class="hashtags">
             ${data[i].hashtags.map(hashtag => `<span class="${hashtag[1]}"><p class="captions">${hashtag[0]}</p></span>`).join('\n            ')}
         </div>
         <p class="post_price">${data[i].price} points</p>
-        <div class="post_actions">
-            <button class="buy-button buy-btn"></button>
-            <div class="post_buttons">
-                <button class="post-button save-button"></button>
-                <button class="post-button share-button"></button>
-            </div>
-        </div>
         `;
         items_grid.appendChild(postCard);
     }
