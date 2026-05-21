09cd738437635ee31c4b9764d674e8a753166519
Maksym <63255725+UnnamedPers@users.noreply.github.com>
2026-05-21 13:47:18 +0200

    feat: implement recommended posts section

--- js/post_parser.js
+++ js/post_parser.js
@@ -17,10 +17,10 @@ function spawnPosts(data) {
 
     main.innerHTML = `
         <header class="post_destination">
-        <p>Category <span style="font-weight: 600;">${post.post_category}</span></p>
-        <a href="index.html" class="back-button">
-          <img src="images/navicons/back icon.svg" alt="Back">
-        </a>
+            <p>Category <span style="font-weight: 600;">${post.post_category}</span></p>
+            <a href="index.html" class="back-button">
+                <img src="images/navicons/back icon.svg" alt="Back">
+            </a>
         </header>
         <div class="post_container">
             <a class="post_images">
@@ -56,7 +56,60 @@ function spawnPosts(data) {
                 </div>
             </div>
         </div>
-        <h2>Description</h1>
+        <h2>Description</h2>
         <p>${post.post_description}</p>
+
+        <h2 style="margin-top: 40px; margin-bottom: 20px;">Other Posts</h2>
+        <div class="items-grid"></div>
     `;
+
+    const items_grid = main.querySelector(".items-grid");
+    
+    for (let i = 0; i < data.length; i++) {
+        if (data[i].id == post_id) continue;
+        
+        const postCard = document.createElement("div");
+        postCard.classList.add("posts_container");
+        postCard.style.cursor = "pointer";
+        postCard.id = data[i].id;
+        postCard.innerHTML = `
+        <a class="post_images">
+            <div class="image-cover">
+                <div class="image-product" style="background-image: url('${data[i].images[0]}');"></div>
+                <div class="black_shadow"></div>
+            </div>
+            <div class="image-cover">
+                <div class="image-product" style="background-image: url('${data[i].images[1]}');"></div>
+                <div class="black_shadow"></div>
+            </div>
+            <div class="image-cover">
+                <div class="image-product" style="background-image: url('${data[i].images[2]}');"></div>
+                <div class="black_shadow"></div>
+            </div>
+        </a>
+        <div class="post_profile">
+            <div class="student_profile_pic" style="background-image: url('${data[i].student_profile_pic}');"></div>
+            <p class="captions">@${data[i].post_username}</p>
+        </div>
+        <h2 class="post_heading">${data[i].item_title}</h2>
+        <div class="hashtags">
+            ${data[i].hashtags ? data[i].hashtags.map(hashtag => `<span class="${hashtag[1]}"><p class="captions">${hashtag[0]}</p></span>`).join('\n            ') : ''}
+        </div>
+        <p class="post_price">${data[i].price} points</p>
+        <div class="post_actions">
+            <button class="buy-button buy-btn"></button>
+            <div class="post_buttons">
+                <button class="post-button save-button"></button>
+                <button class="post-button share-button"></button>
+            </div>
+        </div>
+        `;
+        
+        postCard.addEventListener("click", () => {
+            localStorage.setItem("clicked_post_id", data[i].id);
+            window.location.href = "post.html";
+        });
+
+        items_grid.appendChild(postCard);
+    }
 }
